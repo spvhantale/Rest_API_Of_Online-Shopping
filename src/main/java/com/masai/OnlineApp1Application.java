6e24bc59ebd9de177038a23a10b305ad78c4f2c1
Swapnil Vhantale <103575498+spvhantale@users.noreply.github.com>
2023-02-23 17:19:40 +0530

    Update OnlineApp1Application.java

--- src/main/java/com/masai/OnlineApp1Application.java
+++ src/main/java/com/masai/OnlineApp1Application.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition( 
+	    servers = {
+	       @Server(url = "/", description = "Default Server URL")
+	    }
+	) 
 @EnableSwagger2
 @SpringBootApplication
 public class OnlineApp1Application {
